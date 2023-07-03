805002da313cf027df516637cd6cf15f6ad3d8cc
任孟雪 <renmengxue@jqrjl.com>
2023-07-03 18:12:24 +0800

    update: 首页静态页面搭建

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -4,13 +4,27 @@
     <carousel/>
     <!-- 首页搜索医院的表单 区域 -->
     <search/>
+    <!-- 底部展示医院的结构-->
+    <el-row gutter="20">
+      <el-col :span="20">
+        <!-- 等级子组件 -->
+        <level/>
+        <regin/>
+      </el-col>
+      <el-col :span="4">456</el-col>
+    </el-row>
   </div>
 </template>
 
 <script setup lang="ts">
 //引入首页轮播图组件
 import carousel from './carousel/index.vue'
+//引入首页搜索组件
 import search from './search/index.vue'
+//引入首页等级组件
+import level from './level/index.vue';
+//引入地区组件
+import regin from './regin/index.vue';
 import {} from "vue"
 </script>
 
---
+++ src/pages/home/regin/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="regin">
+    <div class="content">
+      <div class="left">
+        地区
+      </div>
+      <ul class="hospital">
+        <li class="active">全部</li>
+        <li>昌平区</li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { } from "vue"
+</script>
+
+<style lang="scss" scoped>
+.regin{
+   color: #7f7f7f;
+   margin-top:10px;
+   .content{
+    display:flex;
+    .left{
+        margin-right: 10px;
+    }
+    .hospital{
+      display:flex;
+       li {
+        margin-right: 10px;
+        &.active {
+          color: #55a6fe;
+        }
+      }
+      li:hover{
+      color: #55a6fe;
+      cursor: pointer;
+      }
+    }
+   }
+}
+</style>
